2505d3ff3bbd9809f23444b3f196da78a8e8f0fd
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2022-08-25 15:50:28 +0200

    Update latest notes

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,3 +3,27 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+// Copy to clipboard
+window.addEventListener('load', (event) => {
+  const list = document.querySelectorAll('[data-copyToClipboard]');
+
+  list.forEach((element) => {
+    const id = element.getAttribute('data-copyToClipboard');
+
+    element.addEventListener('click', () => {
+      copyToClipboard(id);
+    });
+  });
+});
+
+function copyToClipboard(ID) {
+  /* Get the text field */
+  var copyText = document.getElementById(ID);
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.textContent);
+
+  /* Alert the copied text */
+  alert('تم نسخ الرقم المرجعي: ' + copyText.textContent);
+}
